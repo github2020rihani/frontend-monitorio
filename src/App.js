544cc8caa4b08dedcb056ee3e2d87e25eeca046b
brahimwl <100966574+brahimwl@users.noreply.github.com>
2022-04-25 13:55:36 +0100

    maj

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import Header from './Header';
 import Menu from './Menu';
 import Dashboard from './Dashboard';
 import Footer from './Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import StatUser from './components/Rapport/User';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 import User from './pages/Rapport/User';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div class="wrapper">
-      <Router>
+      
         <Header />
         <Menu />
         <Routes>
           <Route path="/" element={<Dashboard />} ></Route>
           <Route path="/statistique/user" element={<User />} ></Route>
+         
         </Routes>
         <Footer />
-      </Router>
+      
 
     </div>
   );
